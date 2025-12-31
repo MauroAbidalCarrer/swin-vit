b02c29a350e7e90ff4f7dc3c0f7aaf8af87780cd
Mauro Abidal Carrer <mauroabidal@yahoo.fr>
2025-12-31 18:58:25 +0000

    Clean: Removed -q flags.

--- setup_venv.sh
+++ setup_venv.sh
@@ -1,4 +1,4 @@
 python3 -m venv venv
 source venv/bin/activate
-pip install -q torch torchvision datasets accelerate
-pip install -q git+https://github.com/huggingface/transformers.git
+pip install torch torchvision datasets accelerate
+pip install git+https://github.com/huggingface/transformers.git
